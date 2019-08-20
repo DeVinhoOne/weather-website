a795a61273ea06faa9fa16a777ac2ad32f04d8ef
Dawid Łacheta <caushered@gmail.com>
2019-08-20 15:47:49 +0200

    Steup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,12 @@ const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
-const name = 'Dawid';
 
 weatherForm.addEventListener('submit', e => {
    e.preventDefault();
    const location = search.value;
    messageOne.textContent = 'Loading...';
-   fetch('http://localhost:3000/weather?address=' + location).then(response => {
+   fetch('/weather?address=' + location).then(response => {
       response.json().then(data => {
          if (data.error) {
             messageOne.textContent = data.error;
